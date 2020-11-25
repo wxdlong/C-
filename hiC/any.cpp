41e409a7801c260f21c9d14ce13d5d2f797fff8b
wxdlong <wxdlong@qq.com>
2020-11-25 23:08:01 +0800

    init ()

--- hiC/any.cpp
+++ hiC/any.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 int main(){
-    any a = 1, b = string("hello");
+    any a = (1), b = string("hello");
     cout << b.type().name() << ":" << any_cast<string>(b) << endl;
+    cout << a.type().name() << ":" << any_cast<int>(a) << endl;
 }
